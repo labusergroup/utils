a7f34f37a05ab1de733127c65ddde79a32667c1b
labusergroup <labusergroup@users.noreply.github.com>
2017-08-23 18:37:06 -0400

    init

---
+++ dfigure.m
@@ -0,0 +1,10 @@
+function varargout = dfigure(varargin)
+
+if nargin==0
+    hfig = figure('windowstyle','docked');
+else
+    hfig = figure('windowstyle','docked',varargin{1},varargin{2});
+end
+varargout{1}=hfig;
+
+end
